b85fc0c181b81438e0c7ce0b07a0e042db718b2f
thehurricane <ckmccutcheon@gmail.com>
2012-12-07 23:32:39 -0800

    Commented a few things

--- addQuestionsStep1.php
+++ addQuestionsStep1.php
@@ -6,7 +6,7 @@ include 'header.php';
 //TODO: Make this page only accessible by administrators
 ?>
 <form method="post" action="addQuestionsStep2.php">
-<p>Are you trying to add some questions to the system? If so, you're in the right place. First, answer these quick questions please:</p>
+<p>Are you trying to add some questions to the system? If so, you're in the right place. First, please answer these quick questions:</p>
 <p>How many MRVs will this question consist of? (how many steps are there in the mechanism)</p>
 <p><input type="text" name="numberOfQuestionMRVs"/></p>
 <p>What is the largest number of correct MRVs there will be for any given question? (look at the number of correct MRVs there are for each question, and put in the maximum number here)</p>
