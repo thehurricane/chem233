db0487425160b5a8197e52db90db526b1fc21427
thehurricane <ckmccutcheon@gmail.com>
2013-01-17 12:25:09 -0800

    Made a decent version of the adminLogin file

--- adminLogin.php
+++ adminLogin.php
@@ -6,37 +6,20 @@ include 'header.php';
 //Use this line once the program is on the UBC server
 //if (isset($_SESSION['aID']) || isUserValid()) {
 if (isset($_SESSION['aID'])) {
-	//User is already logged in. Do nothing.
+	//Admin is already logged in. Do nothing.
 } else if (isset($_POST['aID'])) {
-	//User has submitted a userID
+	//Admin has submitted an id
 	if (isset($_POST['password'])) {
-		//User has submitted a password
-		if (is_numeric ($_POST['uID'])) {
-			//User has submitted a valid (numeric) id to be checked
-			$uID = $_POST['uID'];
-			
-			/*
-			$query = "SELECT Name, CountryCode FROM City ORDER BY Name LIMIT 20";
-			if ($stmt = mysqli_prepare($link, $query)) {
-				
-				//execute query
-				mysqli_stmt_execute($stmt);
-				
-				//store result
-				mysqli_stmt_store_result($stmt);
-				
-				printf("Number of rows: %d.\n", mysqli_stmt_num_rows($stmt));
-				
-				//free result
-				mysqli_stmt_free_result($stmt);
-				
-				//close statement
-				mysqli_stmt_close($stmt);
-			}
-			*/
-			$uID = $mysqli->real_escape_string($uID);
-			$usersResult = $mysqli->query("SELECT * FROM users WHERE uID = $uID");
-			if (!$usersResult) {
+		//Admin has submitted a password
+		if (is_numeric ($_POST['aID'])) {
+			//Admin has submitted a valid (numeric) id to be checked
+			$aID = $_POST['aID'];
+			$aID = $mysqli->real_escape_string($aID);
+			$password = $_POST['password'];
+			$password = $mysqli->real_escape_string($password);
+			$password = hash('sha256', $password);
+			$adminsResult = $mysqli->query("SELECT * FROM admins WHERE aID = '$aID' AND passwordHash = '$password'");
+			if (!$adminsResult) {
 				//Database problem
 				$pageTitle = "Access Denied";
 				include 'header.php';
@@ -44,30 +27,30 @@ if (isset($_SESSION['aID'])) {
 				include 'footer.php';
 				exit;
 			} else if ($usersResult->num_rows == 0) {
-				//User doesn't exist in the database
+				//Admin doesn't exist in the database
 				$pageTitle = "Access Denied";
 				include 'header.php';
-				echo "<p>Your user ID is incorrect, or you are not a registered user on this site. To try logging in again, click <a href='./index.php'>here</a></p>";
+				echo "<p>Your user ID is incorrect, or you are not a registered admin on this site. To try logging in again, click <a href='./adminLogin.php'>here</a></p>";
 				include 'footer.php';
 				exit;
 			} else {
 				//Set this session variable so it can be accessed throughout the app
-				$usersResultArray = $usersResult->fetch_assoc();
-				$_SESSION['uID'] = $usersResultArray['uID'];
+				$adminsResultArray = $adminsResult->fetch_assoc();
+				$_SESSION['aID'] = $adminsResultArray['aID'];
 			}
 		} else {
-			//User has submitted an invalid id
+			//Admin has submitted an invalid id
 			$pageTitle = "Access Denied";
 			include 'header.php';
-			echo "<p>Please enter a valid number. To try logging in again, click <a href='./index.php'>here</a></p>";
+			echo "<p>Please enter a valid number. To try logging in again, click <a href='./adminLogin.php'>here</a></p>";
 			include 'footer.php';
 			exit;
 		}
 	} else {
-		//User has submitted an invalid id
+		//Admin has submitted an invalid id
 		$pageTitle = "Access Denied";
 		include 'header.php';
-		echo "<p>Please enter a valid number. To try logging in again, click <a href='./index.php'>here</a></p>";
+		echo "<p>Please enter a valid number. To try logging in again, click <a href='./adminLogin.php'>here</a></p>";
 		include 'footer.php';
 		exit;
 	}
